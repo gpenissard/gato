8e6a5aea4c0da07754c93c7fa053d6213cfa4aab
cyiphum <carolina.yiphum@gmail.com>
2017-04-25 12:48:08 -0400

    nouveau clients test

--- db/conn.php
+++ db/conn.php
@@ -1,5 +1,10 @@
 <?php
 require_once ('defines_local.php');
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    require_once('defines_local.php');
+} else { //  $_SERVER['HTTP_HOST'] == "gato.projetisi.com"
+    require_once('defines_sentora.php');
+}
 
 $mysqli = new mysqli(CONN_HOST, CONN_USER, CONN_PWD, DBNAME);
 if ($mysqli->connect_errno) {
@@ -29,6 +34,7 @@ function get_msg($where = '') {
  * @param $content
  */
 function set_msg($user_id, $content) {
+
     global $mysqli;
     $result = false;
     $today = date("Y-m-d H:i:s"); // Donne la date et l'heure dans le système

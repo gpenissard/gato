b644498b1caa217e5cce3c9480a2999f4737113a
krutherf <kevrut83@gmail.com>
2017-04-25 12:48:01 -0400

    afficher msg

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ require_once ("db/conn.php");
 require_once ("_login.php");
 $page_name = "Index";
 require_once ("views/page_top.php");
-
-
 require_once ('views/afficher_message.php');
+if (user_is_logged()){
+    require_once ("views/msg_form.php");
+}
+
 require_once ("views/page_bottom.php")
 ?>
--- views/page_top.php
+++ views/page_top.php
@@ -1,6 +1,6 @@
-<?php
 
-?>
+<?php require_once ("login_form.php");?>
+
 
 
 <!DOCTYPE html>

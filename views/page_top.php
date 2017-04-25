cc30b35f7fc2e9a2c5993017099d6e8da3a7f9ba
edetchev <elorri.detcheverry@gmail.com>
2017-04-25 09:44:30 -0400

    Branchement css

--- views/page_top.php
+++ views/page_top.php
@@ -4,9 +4,10 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="../style/main.css">
 </head>
 <body>

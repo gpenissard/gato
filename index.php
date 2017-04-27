f4d68bf6c7e980bf6d9131adb9cc117f3f1279e4
Gilles Pénissard <gpenissard@gmail.com>
2017-04-27 11:36:54 -0400

    Mettre le formullaire des messages en haut Retirer condition sur affiche messages

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ require_once ("_login.php");
 $page_name = "Index";
 require_once ("views/page_top.php");
 
-
 if (user_is_logged()){
-    require_once ('views/afficher_message.php');
     require_once ("views/msg_form.php");
 }
+
+require_once ('views/afficher_message.php');
 
 require_once ("views/page_bottom.php")
 ?>

e35937f8ece961b008fd53144d886bc23e7a3286
Gilles Pénissard <gpenissard@gmail.com>
2017-04-27 12:41:55 -0400

    Gestion  requête AJAX sur les users

---
+++ ajax/get_messages.php
@@ -0,0 +1,12 @@
+<?php
+/* set out document type to text/javascript instead of text/html */
+header("Content-type: text/javascript");
+header("Access-Control-Allow-Origin: *");
+
+require_once '../db/conn.php';
+require_once  '../db/users.php';
+$messages = get_msg();
+foreach ($messages as $msg) {
+
+}
+echo json_encode();
